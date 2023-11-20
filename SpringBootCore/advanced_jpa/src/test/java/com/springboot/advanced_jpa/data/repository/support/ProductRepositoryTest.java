0f33610a40654072f1356bd635374e7e6ff3cdd7
daradara <devdaradara@gmail.com>
2023-11-20 18:10:09 +0900

    ch08

---
+++ SpringBootCore/advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/support/ProductRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.springboot.advanced_jpa.data.repository.support;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+// 예제 8.39
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void findByNameTest(){
+        List<Product> productList = productRepository.findByName("펜");
+
+        for(Product product : productList){
+            System.out.println(product.getNumber());
+            System.out.println(product.getName());
+            System.out.println(product.getPrice());
+            System.out.println(product.getStock());
+        }
+    }
+
+}
